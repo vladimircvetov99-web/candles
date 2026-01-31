8f7e93c55d132fa9a8cd6ae05516ce5a1773570d
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-01-31 16:06:14 +0600

    finish review block

--- js/main.js
+++ js/main.js
@@ -4,3 +4,30 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', ()=> {
     menuMobile.classList.toggle('menu--open');
 })
+
+
+const swiperOne = new Swiper('.feedback__slider', {
+  loop: false,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+
+const swiperTwo = new Swiper('.certificates__slider', {
+  loop: true,
+   slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+        480: {
+          slidesPerView: 2,
+},
+        360 : {
+            slidesPerView: 1,         
+        }    
+      },
+});
