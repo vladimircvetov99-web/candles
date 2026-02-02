8d033c7564916c256769cd1348684b670d8256f0
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-02-02 18:32:10 +0600

    finish block promo

--- js/main.js
+++ js/main.js
@@ -31,3 +31,26 @@ const swiperTwo = new Swiper('.certificates__slider', {
         }    
       },
 });
+
+const accordeon = document.querySelector('.accordeon');
+const accordeonTitles = accordeon.querySelectorAll('.accordeon__title');
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+accordeonTitle.addEventListener('click', function () {
+
+const currentText = accordeonTitle.parentElement.querySelector('accordeon__text');
+
+accordeonTitle.classList.toggle('accordeon__title--active');
+currentText.classList.toggle('accordeon__text--visible');
+
+if (currentText.classList.contains('accordeon__text--visible')) {
+ currentText.style.maxHeight = currentText.scrollHeight + 'px'
+} else {
+   currentText.style.maxHeight = null;
+  }
+
+
+
+
+  });
+});
